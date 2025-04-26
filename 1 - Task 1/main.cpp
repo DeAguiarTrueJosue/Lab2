80bd78ff28d8853b2f093b375af46129448082d1
lcvtech <itservices@lasallecollegevancouver.com>
2025-04-26 15:00:39 -0700

    04262025

--- 1 - Task 1/main.cpp
+++ 1 - Task 1/main.cpp
@@ -5,18 +5,13 @@ int main() {
 
 	int height = 9;
 
-	for (int i = 1; i <= height; i++) // This line is responsable to say the amount of lines the pyramid is going to use. 1 to height.
+	for (int i = 1; i <= height; i++)
 	{
-		for (int j = 1; j <= height * 2 - 1; j++) // This line is responsable to say the amount of columns the pyramid is going to use. 1 to height * 2 - 1.
+		for (int j = 1; j <= i * 2 + 1; j++)
 		{
-			if (abs(height - j) + abs(i - height) - height < 0) // here, the if is checking if the space in the grid should have a * or not.
+			if (j % 2 == 0)
 			{
-				if (((i % 2) != (j % 2) && height % 2 == 0) || ((i % 2) == (j % 2) && height % 2 == 1)) {
-					printf("%i", i);
-				}
-				else {
-					printf(" ");
-				}
+				printf("%i", i);
 			}
 			else
 			{
